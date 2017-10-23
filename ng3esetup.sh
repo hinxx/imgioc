087e3a27e7393b76b77d6d626f203f471a4e77a6
dev <dev@1x-193-157-201-221.uio.no>
2017-10-23 16:58:18 +0200

    Fix scripts.

--- ng3esetup.sh
+++ ng3esetup.sh
@@ -3,5 +3,7 @@
 export NG3E_TOP=$HOME/git/ng3e
 export NG3E_BASE=$NG3E_TOP/root/R3.15.4/base
 export NG3E_MODULES=$NG3E_TOP/root/R3.15.4/modules
+export NG3E_IOCS=$NG3E_TOP/root/R3.15.4/iocs
 
 export PATH=$PATH:$NG3E_BASE/bin/linux-x86_64
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
