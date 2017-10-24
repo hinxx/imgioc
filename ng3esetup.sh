53b08d912fcca317c7cd87d49b12af962df0eb93
dev <dev@1x-193-157-201-221.uio.no>
2017-10-24 11:33:17 +0200

    add path to libca.so for pyepics

--- ng3esetup.sh
+++ ng3esetup.sh
@@ -5,5 +5,9 @@ export NG3E_BASE=$NG3E_TOP/root/R3.15.4/base
 export NG3E_MODULES=$NG3E_TOP/root/R3.15.4/modules
 export NG3E_IOCS=$NG3E_TOP/root/R3.15.4/iocs
 
+# for EPICS tools
 export PATH=$PATH:$NG3E_BASE/bin/linux-x86_64
+# for aravis lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+# for pyepics
+export PYEPICS_LIBCA=$NG3E_BASE/lib/linux-x86_64/libca.so
